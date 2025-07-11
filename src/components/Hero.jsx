43a0617066aedf0a034ff4ab644d0b288747b913
Your Name <axistay01@example.com>
2025-07-11 02:55:55 +0200

    update some thing now 10/07

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,7 +26,7 @@ function Hero({ onWhatsAppClick, onCallClick }) {
       </div>
 
       <div className="container mx-auto relative z-10 mt-16 md:mt-36">
-        <div className="absolute top-[46vh] lg:top-[32vh] flex justify-center items-center w-full  ">
+        <div className="absolute top-[46vh] lg:top-[36vh] flex justify-center items-center w-full  ">
           <img src="/logo.png" alt="" className=" opacity-30 md:opacity-100  w-60 h-60  md:w-[25%]  lg:h-full " />
 
         </div>
